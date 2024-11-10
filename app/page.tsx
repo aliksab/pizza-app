c345061e4efc0b7400a1578944ac0f83b104b166
aliksab <alexeysavich1112@gmail.com>
2024-11-10 23:53:57 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Title } from "@/components/shared/title";
+import { Container } from "@/components/shared/container";
+import { TopBar } from "@/components/shared/top-bar";
+
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+    </>
+  );
+}
