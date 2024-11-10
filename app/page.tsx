3ec274f1d401f6bd60f0acb418fad932f04fdbe0
aliksab <alexeysavich1112@gmail.com>
2024-11-11 01:20:00 +0300

    add checkbox and range-slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Title } from '@/components/shared/title'
 import { Container } from '@/components/shared/container'
 import { TopBar } from '@/components/shared/top-bar'
+import { Filters } from '@/components/shared/filters'
 
 export default function Home() {
     return (
@@ -9,10 +10,12 @@ export default function Home() {
                 <Title text="Все пиццы" size="lg" className="font-extrabold" />
             </Container>
             <TopBar />
-            <Container className="pb-14">
+            <Container className="mt-10 pb-14">
                 <div className="flex gap=[60px]">
                     {/*Filter */}
-                    <div className="w-[250px]"></div>
+                    <div className="w-[250px]">
+                        <Filters />
+                    </div>
 
                     {/*Products */}
                     <div className="flex-1">
