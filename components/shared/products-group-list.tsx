7a63685db5e804eb5cc4c33f3ed213d03e39fdf4
aliksab <alexeysavich1112@gmail.com>
2024-11-14 22:36:25 +0300

    add proucts-group with react-use

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,5 +1,8 @@
+'use client'
+
+import React, { useEffect, useRef } from 'react'
+import { useIntersection } from 'react-use'
 import { cn } from '@/lib/utils'
-import React from 'react'
 import { Title } from './title'
 import { ProductCard } from './product-card'
 
@@ -18,8 +21,18 @@ export const ProductsGroupList: React.FC<Props> = ({
     className,
     listClassName
 }) => {
+    const intersectionRef = useRef(null)
+    const intersection = useIntersection(intersectionRef, {
+        threshold: 0.4
+    })
+
+    useEffect(() => {
+        if (intersection?.isIntersecting) {
+            console.log(title, categoryId)
+        }
+    }, [categoryId, intersection?.isIntersecting, title])
     return (
-        <div className={className}>
+        <div className={className} id={title} ref={intersectionRef}>
             <Title text={title} size="lg" className="font-extrabold mb-5" />
             <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
                 {items.map((item, i) => (
