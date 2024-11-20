364d2bf5fd1225d77e6fe9bbce545518013ebd2f
aliksab <alexeysavich1112@gmail.com>
2024-11-20 19:53:55 +0300

    add product-id-page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,44 @@
-export default function ProductPage({
+import { Container } from '@/components/shared/container'
+import { GroupVariants } from '@/components/shared/group-variants'
+import { ProductImage } from '@/components/shared/product-image'
+import { Title } from '@/components/shared/title'
+import { prisma } from '@/prisma/prisma-client'
+import { notFound } from 'next/navigation'
+
+export default async function ProductPage({
     params: { id }
 }: {
     params: { id: string }
 }) {
-    return <p>Product {id}</p>
+    const product = await prisma.product.findFirst({
+        where: { id: Number(id) }
+    })
+    if (!product) return notFound()
+    return (
+        <Container className="flex flex-col my-10">
+            <div className="flex flex-1">
+                <ProductImage imageUrl={product.imageUrl} size={40} />
+
+                <div className="w-[490px] bg-[#FCFCFC] p-7">
+                    <Title
+                        text={product.name}
+                        size="md"
+                        className="font-extrabold mb-1"
+                    />
+                    <p className="text-gray-400">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </p>
+                    <GroupVariants
+                        selectedValue="2"
+                        items={[
+                            { name: 'Маленькая', value: '1' },
+                            { name: 'Средняя', value: '2' },
+                            { name: 'Большая', value: '3', disabled: true }
+                        ]}
+                    />
+                </div>
+            </div>
+        </Container>
+    )
 }
